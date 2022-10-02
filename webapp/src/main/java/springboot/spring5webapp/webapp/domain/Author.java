98e05a160c69ef34a9e2ac608ddef7f143584b45
anonymous-oracle <suhass149@outlook.com>
2022-10-02 09:06:59 +0530

    .

--- webapp/src/main/java/springboot/spring5webapp/webapp/domain/Author.java
+++ webapp/src/main/java/springboot/spring5webapp/webapp/domain/Author.java
@@ -1,27 +1,40 @@
-package guru.springframework.spring5webapp.domain;
+package springboot.spring5webapp.webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
 public class Author {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-
+    private Long id;
     private String firstName;
     private String lastName;
-
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
 
     public Author(String firstName, String lastName, Set<Book> books) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
@@ -48,20 +61,20 @@ public class Author {
         this.books = books;
     }
 
-    public long getId() {
-        return id;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return Objects.equals(id, author.id);
     }
 
-    public void setId(long id) {
-        this.id = id;
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
-
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == 0) return true;
-//        if (o == null)
-//    }
-
 
     @Override
     public String toString() {
@@ -71,20 +84,5 @@ public class Author {
                 ", lastName='" + lastName + '\'' +
                 ", books=" + books +
                 '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || this.getClass() != o.getClass()) return false;
-
-        Author author = (Author) o;
-
-        return id == author.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return (int) (id ^ (id >>> 32));
     }
 }
---
+++ webapp/src/test/java/springboot/spring5webapp/webapp/WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.spring5webapp.webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
