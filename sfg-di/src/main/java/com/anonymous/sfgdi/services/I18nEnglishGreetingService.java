c6a879e16b05990ef52c6b947d3a32d4c7d31820
anonymous-oracle <suhass149@outlook.com>
2022-11-06 13:47:02 +0530

    bean life cycle demo

--- sfg-di/src/main/java/com/anonymous/sfgdi/services/I18nEnglishGreetingService.java
+++ sfg-di/src/main/java/com/anonymous/sfgdi/services/I18nEnglishGreetingService.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 
-@Profile("EN")
+@Profile({"EN", "default"}) // specifies the default profile as well; can provide a list of active profiles for use
 @Service("i18nService")
 public class I18nEnglishGreetingService implements GreetingService{
 
